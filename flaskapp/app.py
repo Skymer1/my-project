bb1491bd2d6b5504143ed655d9dd9501f4795f79
Skymer1 <adir465@gmail.com>
2022-08-02 00:38:42 +0300

    Update helm chart

--- flaskapp/app.py
+++ flaskapp/app.py
@@ -4,6 +4,6 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return '<body style="background-color:DarkViolet;"><h1 style="color:Fuchsia;">Ayra is definitely Gay</h1></body>'
+    return '<body style="background-color:DarkGrey;"><h1 style="color:Orange;">BaGit with Shnitzel</h1></body>'
 
 app.run(host='0.0.0.0', port=8081)

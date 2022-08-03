8af734a20e5c3994b682aa7e0b67d5abc09bc841
Skymer1 <adir465@gmail.com>
2022-08-04 00:27:49 +0300

    app.py changes

--- flaskapp/app.py
+++ flaskapp/app.py
@@ -4,6 +4,6 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return '<body style="background-color:DarkGrey;"><h1 style="color:Orange;">BaGit with Shnitzel</h1></body>'
+    return '<body style="background-color:Gray;"><h1 style="color:DarkOrange;">BaGit with Schnitzel</h1></body>'
 
 app.run(host='0.0.0.0', port=8081)
